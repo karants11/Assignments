f1c20a7522002e5825960399dc701a10dd0dd4bb
karants11 <karants11@gmail.com>
2020-10-22 12:09:51 +0530

    Create rightjustify.swift
    Write a function named rightJustify that takes a string named s as a parameter
    and prints the string with enough leading spaces so that the last letter of the string is in column 70 of the display.
    Hint: Use string concatenation and loop. Also, string has a property called length.

---
+++ rightJustify.swift
@@ -0,0 +1,14 @@
+// Assignment 1 Right justify
+
+func rightJustify(s: String) {
+   var spaceNeeded: String = ""
+   var  strLen = 70 - s.length
+    //print(strLen)
+   for _ in 0...strLen-1 {
+       spaceNeeded += " "
+   }
+   //print(spaceNeeded.length)
+   print(spaceNeeded+s)
+
+}
+rightJustify(s: "karan")
