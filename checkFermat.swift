8b9da2bb3b655475611a83f26c9c590bca99fc85
karants11 <karants11@gmail.com>
2020-10-22 12:17:49 +0530

    Create checkFoundation.swift
    Fermat’s Last Theorem says that there are no positive integers a, b, and c such that
    a^n + b^n = c^n
    for any values of n greater than 2.
    Write a function named checkFermat that takes four parameters— a, b, c and n—and checks to see if Fermat’s theorem holds.
    If n is greater than 2 and
    a^n + b^n = c^n
    the program should print, “Holy smokes, Fermat was wrong!” Otherwise the program should print, “No, that doesn’t work.”

---
+++ checkFermat.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func checkFermat(_ a: Int,_ b:Int,_ c: Int,_ n:Int) {
+    if(n > 2) {
+        var lhs = (a ^ n + b ^ n)
+        var rhs = c^n
+        
+		if lhs == rhs{
+            print("Holy smokes, Fermat was wrong!")
+        }
+        else{
+            print("No that doesn’t work.")
+        }
+    }
+    else{
+		print("\(n) is not grater than 2")
+    } 
+}
+
+var n = 3
+var a = 1
+var b = 2
+var c = 2
+
+checkFermat(a,b,c,n)
