d61f3b54f57d46d8c90dc7c971e420a0cf998e4a
karants11 <karants11@gmail.com>
2020-10-22 12:40:33 +0530

    create power.swift
     using the logic x^n = x · x^(n−1)

---
+++ power.swift
@@ -0,0 +1,21 @@
+/*
+Write a recursive function called power that takes a number x and an integer n and returns x^n.
+
+Hint: A recursive definition of this operation is x^n = x · x^(n−1). Also, remember that anything raised to the zeroth power is 1.
+
+Optional challenge: you can make this method more efficient, when n is even, using x^n = (x^(n/2))^2.
+*/
+
+
+import Foundation
+
+func power(_ x: Int, _ n: Int) -> Int {
+    
+	if n == 0 {
+        return 1
+    }
+
+    return ( x * power(x, n-1))
+}
+
+print(power(2, 5))
