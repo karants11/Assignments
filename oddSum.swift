3948f257a0b22776b789a217cc4e1ccd469e09ab
karants11 <karants11@gmail.com>
2020-10-23 14:02:08 +0530

    updated oddSum
    minor updates
    
    Co-Authored-By: karthik-k4 <71215735+karthik-k4@users.noreply.github.com>

--- oddSum.swift
+++ oddSum.swift
@@ -4,6 +4,7 @@ returns the sum of odd integers from 1 to n. Start with a base case,
 and use temporary variables to debug your solution. 
 You might find it helpful to print the value of n each time oddSum is invoked.
 */
+
 /////////////////////////////////
   
 import Foundation
@@ -12,7 +13,7 @@ func oddSum(_ n: Int) -> Int {
     
 	print("current value of n: \(n)")
     
-	if n==1{
+	if n == 1{
         return 1
     }
     return (n + oddSum(n-2))
